587470e1e5cba923e00ddfb72c10278d88548ee2
Esonhugh <esonhughoutside@gmail.com>
2024-12-07 23:38:37 +0800

    update: update default matcher of pv and pod init container

--- pkg/metrics/matcher.go
+++ pkg/metrics/matcher.go
@@ -20,9 +20,12 @@ func DefaultMatchRules() MatchRules {
 		NewMetricMatcher("pod").AddLabel("namespace").AddLabel("pod").AddLabel("node").
 			AddLabel("host_ip").AddLabel("pod_ip"),
 		NewMetricMatcher("container").SetHeader("kube_pod_container_info").AddLabel("namespace").
-			AddLabel("pod").AddLabel("container").AddLabel("image"),
+			AddLabel("pod").AddLabel("container").AddLabel("image_spec").AddLabel("image"),
+		NewMetricMatcher("pod_init_container").AddLabel("namespace").AddLabel("pod").
+			AddLabel("container").AddLabel("image_spec").AddLabel("image"),
+
 		NewMetricMatcher("cronjob").AddLabel("namespace").AddLabel("cronjob").
-			AddLabel("schedule"),
+			AddLabel("schedule").AddLabel("concurrency_policy"),
 
 		NewMetricMatcher("service_account").SetHeader("kube_pod_service_account").
 			AddLabel("namespace").AddLabel("pod").AddLabel("service_account"),
@@ -33,6 +36,11 @@ func DefaultMatchRules() MatchRules {
 			AddLabel("namespace").AddLabel("endpoint").AddLabel("ip"),
 		NewMetricMatcher("endpoint_port").SetHeader("kube_endpoint_ports").
 			AddLabel("namespace").AddLabel("endpoint").AddLabel("port_number"),
+
+		NewMetricMatcher("persistentvolume").AddLabel("persistentvolume").AddLabel("storageclass").
+			AddLabel("gce_persistent_disk_name").AddLabel("ebs_volume_id").AddLabel("azure_disk_name").
+			AddLabel("nfs_server").AddLabel("nfs_path").AddLabel("csi_driver").AddLabel("csi_volume_handle").
+			AddLabel("local_path").AddLabel("local_fs").AddLabel("host_path").AddLabel("host_path_type"),
 	}
 }
 
